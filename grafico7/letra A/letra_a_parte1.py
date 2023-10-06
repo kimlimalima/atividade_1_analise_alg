cac5dcfdf22ad92bac4403c4b2beb20e0c160a00
Kim Lima <kimlimaufpa@gmail.com>
2023-10-06 20:56:25 -0300

    Alterações

--- grafico7/letra A/letra_a_parte1.py
+++ grafico7/letra A/letra_a_parte1.py
@@ -3,21 +3,16 @@ import numpy as np
 
 n = np.arange(1, 100)
 
+log_n = 3*np.log10(n**2)+2
 
-fn = 3*n**3 + 20*n**2 + 5
+n_cubico = 8*np.log10(n)
 
-gn = 3 * np.log10
-
-hn = n**2
-
-
-plt.plot(n, fn, label='f(n)', linestyle='-', marker='o')
-plt.plot(n, gn, label='g(n)', linestyle='--', marker='x')
-plt.plot(n, hn, label='h(n)', linestyle='--', marker='x')
+plt.plot(n, log_n, label='g(n)', linestyle='-', marker='o')
+plt.plot(n, n_cubico, label='h(n)', linestyle='--', marker='x')
 
 plt.xlabel('n')
 plt.ylabel('Valor')
-plt.title('Comparação entre 3*log10(n**2)+2 e log10(n)')
+plt.title('Comparação entre g(n) e h(n)')
 
 plt.legend()
 plt.savefig("parte1.jpg", dpi=600)
--- grafico7/letra A/letra_a_parte2.py
+++ grafico7/letra A/letra_a_parte2.py
@@ -7,12 +7,12 @@ log_n = 3*np.log10(n**2)+2
 
 n_cubico = np.log10(n)
 
-plt.plot(n, log_n, label='3*log10(n**2)+2', linestyle='-', marker='o')
+plt.plot(n, log_n, label='g(n)', linestyle='-', marker='o')
 plt.plot(n, n_cubico, label='log10(n)', linestyle='--', marker='x')
 
 plt.xlabel('n')
 plt.ylabel('Valor')
-plt.title('Comparação entre 3*log10(n**2)+2 e log10(n)')
+plt.title('Comparação entre g(n) e log10(n)')
 
 plt.legend()
 plt.savefig("parte2.jpg", dpi=600)
